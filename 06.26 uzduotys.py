e1c472da30f36da0e42370333ac1d893fa564f15
Tomas <tomas.abramavicius@gmail.com>
2023-06-28 11:57:55 +0300

    initial commit

--- 06.26 uzduotys.py
+++ 06.26 uzduotys.py
@@ -1,32 +1,33 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-
-#employees.csv
-# * Apskaičiuoti vidurkį, sumą, minimumą, maksimumą ir kitus statistinius rodiklius stulpeliuose arba grupėse
-#naudojant mean(), sum(), min(), max() ir kitas funkcijas.
-
-df = pd.read_csv('employees.csv')
-#print(df.head(5))
-
-group_average = df.groupby('FIRST_NAME')['SALARY'].mean()
-print(group_average)
-
-
-#group_sizes = df.groupby('FIRST_NAME').size()
-# print(group_sizes)
-
-#group_average = df.groupby('FIRST_NAME')['EMPLOYEE_ID'].mean()
+# import pandas as pd
+# from matplotlib import pyplot as plt
+#
+# #employees.csv
+# # * Apskaičiuoti vidurkį, sumą, minimumą, maksimumą ir kitus statistinius rodiklius stulpeliuose arba grupėse
+# #naudojant mean(), sum(), min(), max() ir kitas funkcijas.
+#
+# df = pd.read_csv('employees.csv')
+# #print(df.head(5))
+#
+# group_average = df.groupby('FIRST_NAME')['SALARY'].mean()
 # print(group_average)
-
-#group_max = df.groupby('FIRST_NAME')['EMPLOYEE_ID'].max()
-# print(group_max)
-
-group_average.plot(kind='line', figsize=(8, 4))
-# pridedamos diagramos antrastes
-plt.title('Suvestines statistika pagal vardus ir ju atlyginimus')
-plt.xlabel('Vardas')
-plt.ylabel('Atlyginimas')
-
-# atvaizduojama diagrama
-plt.show()
-
+#
+#
+# #group_sizes = df.groupby('FIRST_NAME').size()
+# # print(group_sizes)
+#
+# #group_average = df.groupby('FIRST_NAME')['EMPLOYEE_ID'].mean()
+# # print(group_average)
+#
+# #group_max = df.groupby('FIRST_NAME')['EMPLOYEE_ID'].max()
+# # print(group_max)
+#
+# group_average.plot(kind='line', figsize=(8, 4))
+# # pridedamos diagramos antrastes
+# plt.title('Suvestines statistika pagal vardus ir ju atlyginimus')
+# plt.xlabel('Vardas')
+# plt.ylabel('Atlyginimas')
+#
+# # atvaizduojama diagrama
+# plt.show()
+#
+# # uzduotys nebaigtos
---
+++ 06.27.sales_data.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Suskirstykite klientus pagal šalį.
+# Atrinkite užsakymus, kurių suma viršija 1000 eurų
+# Išfiltruokite užsakymus, kurie buvo pristatyti nuo 2003/9/30 iki 2005/03/15 .
+# Išfiltruokite užsakymus, kurių statusas 'Disputed';
+# Sukurkite skritulinę diagramą, kurioje būtų pavaizduota klientų skaičiaus pasiskirstymas pagal šalis
+
+#df = pd.read_csv('sales_data_sample.csv', encoding="ISO-8859-1")
+# print(df.head(5))
+#
+# group_country = df.groupby('COUNTRY')
+# print(group_country)
+#
+# plt.figure(figsize=(12, 6))
+# group_country.size().plot(kind='bar')
+# plt.title('salys pagal uzsakyma')
+# plt.xlabel('salis')
+# plt.ylabel('klientai')
+# plt.show()
+
+#2.
+
+# group_by_order = df[df['SALES'] > 1000]
+# print(group_by_order)
+
+# df['total'] = df['QUANTITYORDERED'] * df['PRICEEACH']
+# group_by_order = df[df['total'] > 5000][['CUSTOMERNAME', 'total']]
+# df.to_csv('sales_data_sample.csv', index=False)
+# print(group_by_order)
+
+# 3. Išfiltruokite užsakymus, kurie buvo pristatyti nuo 2003/9/30 iki 2005/03/15 .
+
+# df['ORDERDATE']=pd.to_datetime(df['ORDERDATE'], format='%d/%m/%Y %H:%M', errors='coerce') #format datos eiliskumas
+# datu_filtravimas = df[(df['ORDERDATE'] >= '9/9/2003') & (df['ORDERDATE'] <= '3/3/2005')]
+# print(datu_filtravimas)
+
+# kitas budas datos format:
+# df["ORDERDATE"] = pd.to_datetime(df["ORDERDATE"])
+# start_date = '2003-09-30'
+# end_date = '2005-03-15'
+# order_date = df[df['ORDERDATE'].between(start_date, end_date)]
+
+#4. Išfiltruokite užsakymus, kurių statusas 'Disputed' NEBAIGTA
+
+#disputed = status
+
+#5(1).Sukurkite skritulinę diagramą, kurioje būtų pavaizduota klientų skaičiaus pasiskirstymas pagal šalis
+
+# group_country = df['COUNTRY'].value_counts().nlargest(5)
+#
+# plt.bar(group_country.index, group_country.values)
+#
+# plt.title('salys pagal uzsakyma')
+# plt.xlabel('salis')
+# plt.ylabel('klientai')
+# plt.xticks(rotation=90)
+# plt.show()
+#__________________________________________________________________________
+
